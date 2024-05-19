dd68ab767c9304f3eba82acdc9a13287ac7cc855
youjin09222 <youjin09222@gmail.com>
2024-05-19 15:01:27 +0900

    #1 [SETTING] : 기본 setting

---
+++ app/src/main/java/com/talkable/presentation/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.talkable.presentation
+
+import com.talkable.R
+import com.talkable.core.base.BindingActivity
+import com.talkable.databinding.ActivityMainBinding
+
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    override fun initView() {
+    }
+}
