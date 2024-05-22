ab574021c81984c53435e30bbf3fe15b6dbd987e
youjin09222 <youjin09222@gmail.com>
2024-05-22 21:12:33 +0900

    #6 [UI] : 로그인 UI 구현

--- app/src/main/java/com/talkable/TalkableApp.kt
+++ app/src/main/java/com/talkable/TalkableApp.kt
@@ -1,12 +1,16 @@
 package com.talkable
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import timber.log.Timber
 
 class TalkableApp : Application() {
     override fun onCreate() {
         super.onCreate()
         setTimber()
+
+        // Kakao SDK 초기화
+        KakaoSdk.init(this, "{NATIVE_APP_KEY}")
     }
 
     private fun setTimber() {
