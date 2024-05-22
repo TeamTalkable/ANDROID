2c47e565f201f7deaac1e1b01face97aefec5997
Sohyun <so405@naver.com>
2024-05-22 20:35:29 +0900

    #5 [SETTING] : kapt 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlinx.serialization)
+    kotlin("kapt")
     id("org.jetbrains.kotlin.plugin.parcelize")
 }
 
