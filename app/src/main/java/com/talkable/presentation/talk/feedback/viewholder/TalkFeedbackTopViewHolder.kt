eac29ba272d92ac0ae1f54c22fc66ec7a475c857
Sohyun <so405@naver.com>
2024-05-22 20:55:00 +0900

    #5 [FEAT] : bindingadapter 적용

--- app/src/main/java/com/talkable/presentation/talk/feedback/viewholder/TalkFeedbackTopViewHolder.kt
+++ app/src/main/java/com/talkable/presentation/talk/feedback/viewholder/TalkFeedbackTopViewHolder.kt
@@ -1,8 +1,6 @@
 package com.talkable.presentation.talk.feedback.viewholder
 
 import androidx.recyclerview.widget.RecyclerView
-import coil.load
-import com.talkable.R
 import com.talkable.databinding.ItemTalkFeedbackTopBinding
 import com.talkable.presentation.talk.feedback.model.TalkFeedbackModel
 
@@ -11,7 +9,6 @@ class TalkFeedbackTopViewHolder(private val binding: ItemTalkFeedbackTopBinding)
     fun onBind(data: TalkFeedbackModel) = with(binding) {
         model = data
         executePendingBindings()
-        //bindingadapter 곧 추가하고 바꿀거임
-        binding.ivTalkFeedbackTopFlower.setImageResource(R.drawable.background_splash_screen)
     }
 }
+
