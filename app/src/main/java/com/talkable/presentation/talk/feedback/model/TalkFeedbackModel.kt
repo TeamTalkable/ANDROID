a7fc545b0aec3dc9d3b325e0032664ff7b32f5d6
Sohyun <so405@naver.com>
2024-05-22 20:18:24 +0900

    #5 [FEAT] : recyclerview 연결

---
+++ app/src/main/java/com/talkable/presentation/talk/feedback/model/TalkFeedbackModel.kt
@@ -0,0 +1,40 @@
+package com.talkable.presentation.talk.feedback.model
+
+data class TalkFeedbackModel(
+    val talkFeedbackId: Int,
+    val talkTime: String,
+    val flowerImage: String,
+    val remainTime: String,
+    val learnedExpression: List<Learned.Expression>,
+    val learnedGrammar: List<Learned.Grammar>,
+    val learnedPronunciation: List<Learned.Pronunciation>,
+    val isGraphChanged: Boolean,
+)
+
+sealed class Learned {
+    abstract val type: String
+
+    data class Label(
+        override val type: String,
+    ) : Learned()
+
+    data class Expression(
+        override val type: String,
+        val wordEnglish: String,
+        val wordKorean: String,
+    ) : Learned()
+
+    data class Grammar(
+        override val type: String,
+        val wrongGrammar: String,
+        val correctGrammar: String,
+        val reason: String,
+    ) : Learned()
+
+    data class Pronunciation(
+        override val type: String,
+        val word: String,
+    ) : Learned()
+}
+
+
