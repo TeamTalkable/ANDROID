4ced5e30e2403ef28be248ba58c7b812acfec830
youjin09222 <youjin09222@gmail.com>
2024-06-05 16:04:57 +0900

    #10 [FEAT] : 코치 마크 View Control

--- app/src/main/java/com/talkable/presentation/talk/TalkFragment.kt
+++ app/src/main/java/com/talkable/presentation/talk/TalkFragment.kt
@@ -1,11 +1,22 @@
 package com.talkable.presentation.talk
 
+import android.view.View.GONE
 import com.talkable.R
 import com.talkable.core.base.BindingFragment
 import com.talkable.databinding.FragmentTalkBinding
+import com.talkable.presentation.MainActivity
 
 class TalkFragment : BindingFragment<FragmentTalkBinding>(R.layout.fragment_talk) {
     override fun initView() {
-        // 로직 작성
+        (activity as? MainActivity)?.hideBottomNavigation()
+        initGuideLayoutClickListener()
+    }
+
+    // 코치 마크 레이아웃
+    private fun initGuideLayoutClickListener() {
+        val layoutGuide = binding.includeLayoutTalkGuide.layoutTalkGuide
+        layoutGuide.setOnClickListener {
+            layoutGuide.visibility = GONE
+        }
     }
 }
