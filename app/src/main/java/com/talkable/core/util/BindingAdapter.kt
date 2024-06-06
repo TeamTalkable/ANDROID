259d93031840395ebf07197ed569d17a39734a32
Sohyun <so405@naver.com>
2024-06-06 14:15:40 +0900

    #13 [UI] : progress bar 비율 설정

--- app/src/main/java/com/talkable/core/util/BindingAdapter.kt
+++ app/src/main/java/com/talkable/core/util/BindingAdapter.kt
@@ -1,6 +1,9 @@
 package com.talkable.core.util
 
+import android.view.View
 import android.widget.ImageView
+import android.widget.SeekBar
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
 import coil.load
 import coil.transform.RoundedCornersTransformation
@@ -35,4 +38,34 @@ fun setTint(view: ImageView, color: Int) {
     } else {
         view.clearColorFilter()
     }
+}
+
+@BindingAdapter("setProgressRatio")
+fun setProgressRatio(view: SeekBar, studyTime: String) {
+    view.progress = when (studyTime.toInt()) {
+        in 1..19 -> 11
+        20 -> 25
+        in 21..39 -> 37
+        40 -> 50
+        in 41..69 -> 63
+        70 -> 75
+        in 71..99 -> 89
+        else -> studyTime.toInt()
+    }
+}
+
+@BindingAdapter("setProgressRatio")
+fun setConstraintBias(view: View, studyTime: String) {
+    val layoutParams = view.layoutParams as? ConstraintLayout.LayoutParams
+    layoutParams?.horizontalBias = when (studyTime.toInt()) {
+        in 1..19 -> 0.125F
+        20 -> 0.25F
+        in 21..39 -> 0.375F
+        40 -> 0.5F
+        in 41..69 -> 0.625F
+        70 -> 0.75F
+        in 71..99 -> 0.875F
+        else -> studyTime.toFloat().div(100)
+    }
+    view.layoutParams = layoutParams
 }
