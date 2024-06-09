eb44b6fa6fb4834c2cd60f79af2299c15e9de930
Sohyun <so405@naver.com>
2024-06-10 03:49:00 +0900

    #13 [FEAT] : flower list에서 detail로 이동

--- app/src/main/java/com/talkable/presentation/myflower/MyFlowerDetailFragment.kt
+++ app/src/main/java/com/talkable/presentation/myflower/MyFlowerDetailFragment.kt
@@ -3,6 +3,7 @@ package com.talkable.presentation.myflower
 import com.talkable.R
 import com.talkable.core.base.BindingFragment
 import com.talkable.databinding.FragmentMyFlowerDetailBinding
+import com.talkable.presentation.myflower.MyFlowerListFragment.Companion.FLOWER_ID
 import com.talkable.presentation.myflower.model.MyFlowerGetDate
 import com.talkable.presentation.myflower.model.MyFlowerItem
 import com.talkable.presentation.myflower.model.MyFlowerItemType
@@ -12,44 +13,101 @@ import timber.log.Timber
 class MyFlowerDetailFragment :
     BindingFragment<FragmentMyFlowerDetailBinding>(R.layout.fragment_my_flower_detail) {
     override fun initView() {
-        binding.model = mockData
-        binding.layoutMyFlowerProgress.model = mockData
-        initMyFlowerItemAdapter()
+        binding.model = getFlowerModelData()
+        binding.layoutMyFlowerProgress.model = getFlowerModelData()
+        initMyFlowerItemAdapter(getFlowerModelData())
         setSeekbarDisabled()
     }
 
-    private fun initMyFlowerItemAdapter() {
-        binding.rvMyFlowerItem.adapter = MyFlowerDetailItemAdapter(requireContext()).apply {
-            submitList(mockData.itemData)
+    private fun getFlowerModelData(): MyFlowerModel? {
+        return if (getFlowerId() != null) {
+            mockData.find { it.flowerId == getFlowerId() }
+        } else {
+            mockData.getOrNull(0)
         }
     }
 
-    private fun setSeekbarDisabled(){
+    private fun getFlowerId() = arguments?.getInt(FLOWER_ID)
+
+    private fun initMyFlowerItemAdapter(data: MyFlowerModel?) {
+        binding.rvMyFlowerItem.adapter = MyFlowerDetailItemAdapter(requireContext()).apply {
+            submitList(data?.itemData)
+        }
+    }
+
+    private fun setSeekbarDisabled() {
         binding.layoutMyFlowerProgress.sbMyFlower.isEnabled = false
     }
 
     companion object {
-        val mockData = MyFlowerModel(
-            flowerId = 1,
-            flowerImage = "https://github.com/TeamTalkable/ANDROID/assets/98076050/2aea7e4e-cea5-44bd-882d-5ffe749de174",
-            flowerName = "코스모스",
-            flowerStartDate = "2024.05.05",
-            flowerEndDate = null,
-            studyTime = "60",
-            getDateData = listOf(
-                MyFlowerGetDate(time = 0, "05.09"),
-                MyFlowerGetDate(time = 20, "05.14"),
-                MyFlowerGetDate(time = 40, "05.20"),
-                MyFlowerGetDate(time = 70),
-                MyFlowerGetDate(time = 100),
+        val mockData = listOf(
+            MyFlowerModel(
+                flowerId = 3,
+                flowerImage = "https://github.com/TeamTalkable/ANDROID/assets/98076050/2aea7e4e-cea5-44bd-882d-5ffe749de174",
+                flowerName = "코스모스",
+                flowerStartDate = "2024.05.05",
+                flowerEndDate = null,
+                studyTime = "60",
+                getDateData = listOf(
+                    MyFlowerGetDate(time = 0, "05.09"),
+                    MyFlowerGetDate(time = 20, "05.14"),
+                    MyFlowerGetDate(time = 40, "05.20"),
+                    MyFlowerGetDate(time = 70),
+                    MyFlowerGetDate(time = 100),
+                ),
+                itemData = listOf(
+                    MyFlowerItem(itemType = MyFlowerItemType.SUN, count = 1),
+                    MyFlowerItem(itemType = MyFlowerItemType.COMPOST),
+                    MyFlowerItem(itemType = MyFlowerItemType.WATER, count = 4),
+                    MyFlowerItem(itemType = MyFlowerItemType.MEDICINE),
+                    MyFlowerItem(itemType = MyFlowerItemType.WIND),
+                    MyFlowerItem(itemType = MyFlowerItemType.SHOVEL),
+                )
             ),
-            itemData = listOf(
-                MyFlowerItem(itemType = MyFlowerItemType.SUN, count = 1),
-                MyFlowerItem(itemType = MyFlowerItemType.COMPOST),
-                MyFlowerItem(itemType = MyFlowerItemType.WATER, count = 4),
-                MyFlowerItem(itemType = MyFlowerItemType.MEDICINE),
-                MyFlowerItem(itemType = MyFlowerItemType.WIND),
-                MyFlowerItem(itemType = MyFlowerItemType.SHOVEL),
+            MyFlowerModel(
+                flowerId = 2,
+                flowerImage = "https://github.com/TeamDon-tBe/ANDROID/assets/98076050/277a2c24-1523-4704-93fe-f8aca5bf34f2",
+                flowerName = "장미꽃",
+                flowerStartDate = "2024.04.09",
+                flowerEndDate = "04.20",
+                studyTime = "100",
+                getDateData = listOf(
+                    MyFlowerGetDate(time = 0, "04.09"),
+                    MyFlowerGetDate(time = 20, "04.15"),
+                    MyFlowerGetDate(time = 40, "04.16"),
+                    MyFlowerGetDate(time = 70, "04.23"),
+                    MyFlowerGetDate(time = 100, "04.30"),
+                ),
+                itemData = listOf(
+                    MyFlowerItem(itemType = MyFlowerItemType.SUN, count = 1),
+                    MyFlowerItem(itemType = MyFlowerItemType.COMPOST),
+                    MyFlowerItem(itemType = MyFlowerItemType.WATER, count = 1),
+                    MyFlowerItem(itemType = MyFlowerItemType.MEDICINE),
+                    MyFlowerItem(itemType = MyFlowerItemType.WIND, count = 1),
+                    MyFlowerItem(itemType = MyFlowerItemType.SHOVEL),
+                )
+            ), MyFlowerModel(
+                flowerId = 1,
+                flowerImage = "https://github.com/TeamDon-tBe/ANDROID/assets/98076050/6b6cecc7-4151-468f-926f-5de838dd17bd",
+                flowerName = "보라꽃",
+                flowerStartDate = "2024.04.09",
+                flowerEndDate = "04.20",
+                studyTime = "100",
+                getDateData = listOf(
+                    MyFlowerGetDate(time = 0, "04.09"),
+                    MyFlowerGetDate(time = 20, "04.15"),
+                    MyFlowerGetDate(time = 40, "04.16"),
+                    MyFlowerGetDate(time = 70, "04.23"),
+                    MyFlowerGetDate(time = 100, "04.30"),
+                ),
+                itemData = listOf(
+                    MyFlowerItem(itemType = MyFlowerItemType.SUN, count = 1),
+                    MyFlowerItem(itemType = MyFlowerItemType.COMPOST),
+                    MyFlowerItem(itemType = MyFlowerItemType.WATER),
+                    MyFlowerItem(itemType = MyFlowerItemType.MEDICINE),
+                    MyFlowerItem(itemType = MyFlowerItemType.WIND),
+                    MyFlowerItem(itemType = MyFlowerItemType.SHOVEL),
+                )
             )
         )
     }
--- app/src/main/java/com/talkable/presentation/myflower/MyFlowerListFragment.kt
+++ app/src/main/java/com/talkable/presentation/myflower/MyFlowerListFragment.kt
@@ -1,5 +1,7 @@
 package com.talkable.presentation.myflower
 
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
 import com.talkable.R
 import com.talkable.core.base.BindingFragment
 import com.talkable.databinding.FragmentMyFlowerListBinding
@@ -14,10 +16,17 @@ class MyFlowerListFragment :
     private fun initMyFlowerListAdapter() {
         binding.rvMyFlowerList.adapter =
             MyFlowerListAdapter(requireContext(), onClickMyFlowerItem = {
-
+                navigateToMyFlowerDetailFragment(it)
             }).apply {
                 submitList(mockData)
             }
+    }
+
+    private fun navigateToMyFlowerDetailFragment(flowerId: Int) {
+        findNavController().navigate(
+            R.id.action_my_flower_to_my_flower_detail,
+            bundleOf(FLOWER_ID to flowerId)
+        )
     }
 
     companion object {
@@ -31,5 +40,7 @@ class MyFlowerListFragment :
                 flowerImage = "https://github.com/TeamTalkable/ANDROID/assets/98076050/45b692ac-2fb1-45d1-b3b1-3d887c08928a"
             )
         )
+
+        const val FLOWER_ID = "flowerId"
     }
 }
