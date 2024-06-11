88191de0332d85e900bf0684890ad76a38b34cc2
Sohyun <so405@naver.com>
2024-06-11 15:46:28 +0900

    #9 [UI] : 발음 아이콘 selector 적용

--- app/src/main/java/com/talkable/presentation/talk/feedback/viewholder/TalkFeedbackLearnedPronunciationViewHolder.kt
+++ app/src/main/java/com/talkable/presentation/talk/feedback/viewholder/TalkFeedbackLearnedPronunciationViewHolder.kt
@@ -6,9 +6,23 @@ import com.talkable.presentation.talk.feedback.model.Learned
 
 class TalkFeedbackLearnedPronunciationViewHolder(private val binding: ItemTalkFeedbackLearnedPronunciationBinding) :
     RecyclerView.ViewHolder(binding.root) {
+
+    init {
+        with(binding) {
+            ivTalkLearnedPronunciationAiVoice.setOnClickListener {
+                ivTalkLearnedPronunciationAiVoice.isSelected =
+                    !ivTalkLearnedPronunciationAiVoice.isSelected
+            }
+
+            ivTalkLearnedPronunciationUserVoice.setOnClickListener {
+                ivTalkLearnedPronunciationUserVoice.isSelected =
+                    !ivTalkLearnedPronunciationUserVoice.isSelected
+            }
+        }
+    }
+
     fun onBind(data: Learned.Pronunciation) = with(binding) {
         model = data
         executePendingBindings()
     }
-
 }
