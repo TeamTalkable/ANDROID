ec6ea80371575de2e302663e9d12116e9a0e8bef
Sohyun <so405@naver.com>
2024-07-17 22:43:40 +0900

    #36 [FEAT] : 다이얼로그 키 값 상수화

--- app/src/main/java/com/talkable/core/util/Const.kt
+++ app/src/main/java/com/talkable/core/util/Const.kt
@@ -3,3 +3,8 @@ package com.talkable.core.util
 object Key {
     const val CHART_KEY = "chart"
 }
+
+object DialogKey {
+    const val LOGOUT_DIALOG = "logout"
+    const val WITHDRAW_DIALOG = "withdraw"
+}
