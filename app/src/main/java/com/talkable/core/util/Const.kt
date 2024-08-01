e192c8c072ca04a5e47141793789827ddb863edd
Sohyun <so405@naver.com>
2024-08-01 15:04:47 +0900

    #36 [ADD] : string 및 const 추가

--- app/src/main/java/com/talkable/core/util/Const.kt
+++ app/src/main/java/com/talkable/core/util/Const.kt
@@ -3,9 +3,11 @@ package com.talkable.core.util
 object Key {
     const val CHART_KEY = "chart"
     const val ONBOARDING_KEY = "onboarding"
+    const val USER_KEY = "user"
 }
 
 object DialogKey {
     const val LOGOUT_DIALOG = "logout"
     const val WITHDRAW_DIALOG = "withdraw"
+    const val ALARM_SETTING_DIALOG = "alarmSetting"
 }
