2d618b650f42c7b242c5fe8447ea387f5a34b410
Sohyun <so405@naver.com>
2024-08-01 20:00:37 +0900

    #39 [FEAT] : const

--- app/src/main/java/com/talkable/core/util/Const.kt
+++ app/src/main/java/com/talkable/core/util/Const.kt
@@ -2,4 +2,10 @@ package com.talkable.core.util
 
 object Key {
     const val CHART_KEY = "chart"
+    const val QUIZ_KEY = "quiz"
+}
+
+object DialogKey {
+    const val QUIZ_DIALOG = "quiz"
+    const val QUIZ_ERROR_DIALOG = "quiz_error"
 }
