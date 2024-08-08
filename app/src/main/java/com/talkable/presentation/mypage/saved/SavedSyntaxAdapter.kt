f875f25435c80cf15565336c9659b819318a9afb
youjin09222 <youjin09222@naver.com>
2024-08-08 21:54:42 +0900

    #37 [REFACTOR] : ListAdapter로 변경

--- app/src/main/java/com/talkable/presentation/mypage/saved/SavedSyntaxAdapter.kt
+++ app/src/main/java/com/talkable/presentation/mypage/saved/SavedSyntaxAdapter.kt
@@ -1,20 +1,28 @@
 package com.talkable.presentation.mypage.saved
 
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import com.talkable.presentation.mypage.saved.model.SavedWord
 
-class SavedSyntaxAdapter(
-    private val savedSyntaxList: List<SavedWord>
-) : RecyclerView.Adapter<SavedSyntaxViewHolder>() {
+class SavedSyntaxAdapter :
+    ListAdapter<SavedWord, SavedSyntaxViewHolder>(SavedSyntaxDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SavedSyntaxViewHolder {
         return SavedSyntaxViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: SavedSyntaxViewHolder, position: Int) {
-        holder.onBind(savedSyntaxList[position])
+        holder.onBind(getItem(position))
+    }
+}
+
+class SavedSyntaxDiffCallback : DiffUtil.ItemCallback<SavedWord>() {
+    override fun areItemsTheSame(oldItem: SavedWord, newItem: SavedWord): Boolean {
+        return oldItem.word == newItem.word
     }
 
-    override fun getItemCount() = savedSyntaxList.size
+    override fun areContentsTheSame(oldItem: SavedWord, newItem: SavedWord): Boolean {
+        return oldItem == newItem
+    }
 }
--- app/src/main/java/com/talkable/presentation/mypage/saved/SavedWordAdapter.kt
+++ app/src/main/java/com/talkable/presentation/mypage/saved/SavedWordAdapter.kt
@@ -1,20 +1,27 @@
 package com.talkable.presentation.mypage.saved
 
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import com.talkable.presentation.mypage.saved.model.SavedWord
 
-class SavedWordAdapter(
-    private val savedWordList: List<SavedWord>
-) : RecyclerView.Adapter<SavedWordViewHolder>() {
+class SavedWordAdapter : ListAdapter<SavedWord, SavedWordViewHolder>(SavedWordDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SavedWordViewHolder {
         return SavedWordViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: SavedWordViewHolder, position: Int) {
-        holder.onBind(savedWordList[position])
+        holder.onBind(getItem(position))
+    }
+}
+
+class SavedWordDiffCallback : DiffUtil.ItemCallback<SavedWord>() {
+    override fun areItemsTheSame(oldItem: SavedWord, newItem: SavedWord): Boolean {
+        return oldItem.word == newItem.word
     }
 
-    override fun getItemCount() = savedWordList.size
+    override fun areContentsTheSame(oldItem: SavedWord, newItem: SavedWord): Boolean {
+        return oldItem == newItem
+    }
 }
