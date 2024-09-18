db474c922bf225070a10e5a0a936554634c6dca7
youjin09222 <youjin09222@naver.com>
2024-09-18 21:03:02 +0900

    #53 [FEAT] : 피드백 버튼 리스너 설정

--- app/src/main/java/com/talkable/presentation/feedback/today/TodayFeedbackViewHolder.kt
+++ app/src/main/java/com/talkable/presentation/feedback/today/TodayFeedbackViewHolder.kt
@@ -10,14 +10,21 @@ import com.talkable.presentation.feedback.today.model.TodayFeedback
 
 abstract class FeedbackViewHolder<T>(itemView: View) : RecyclerView.ViewHolder(itemView) {
     abstract fun onBind(feedbackData: T, isLastItem: Boolean, isOnlyItem: Boolean)
-    protected fun initItemViewClickListener(vararg views: View) {
+    protected fun initItemViewClickListener(feedbackView: View, vararg views: View) {
         itemView.setOnClickListener {
             views.forEach { view ->
                 view.visibility = if (view.visibility == View.GONE) View.VISIBLE else View.GONE
             }
+            feedbackView.isSelected = !feedbackView.isSelected
         }
     }
-    protected fun handleVisibility(isOnlyItem: Boolean, isLastItem: Boolean, circleView: View, dividerView: View) {
+
+    protected fun handleVisibility(
+        isOnlyItem: Boolean,
+        isLastItem: Boolean,
+        circleView: View,
+        dividerView: View
+    ) {
         circleView.visibility = if (isOnlyItem) View.GONE else View.VISIBLE
         dividerView.visibility = if (isOnlyItem || isLastItem) View.GONE else View.VISIBLE
     }
@@ -25,19 +32,34 @@ abstract class FeedbackViewHolder<T>(itemView: View) : RecyclerView.ViewHolder(i
 
 class FeedbackExpressionViewHolder(private val binding: ItemTodayFeedbackExpressionBinding) :
     FeedbackViewHolder<TodayFeedback.Expression>(binding.root) {
-
     init {
-        initItemViewClickListener(binding.spaceTodayFeedback, binding.layoutTodayFeedback.layoutTodayFeedback)
+        with(binding) {
+            initItemViewClickListener(
+                layoutTodayFeedbackExpression.btnTalkFeedback,
+                spaceTodayFeedback,
+                layoutTodayFeedback.layoutTodayFeedback
+            )
+        }
     }
 
-    override fun onBind(feedbackData: TodayFeedback.Expression, isLastItem: Boolean, isOnlyItem: Boolean) {
+    override fun onBind(
+        feedbackData: TodayFeedback.Expression,
+        isLastItem: Boolean,
+        isOnlyItem: Boolean
+    ) {
         with(binding) {
+            layoutTodayFeedbackExpression.btnTalkFeedback.visibility = View.VISIBLE
             layoutTodayFeedbackExpression.tvTalkFeedbackLearnedExpressionEnglish.text = feedbackData.english
             layoutTodayFeedbackExpression.tvTalkFeedbackLearnedExpressionKorean.text = feedbackData.translation
             layoutTodayFeedback.tvTodayFeedbackBefore.text = feedbackData.feedbackBefore
             layoutTodayFeedback.tvTodayFeedbackAfter.text = feedbackData.feedbackAfter
 
-            handleVisibility(isOnlyItem, isLastItem, viewTodayFeedbackCircle, dividerTodayFeedbackExpression)
+            handleVisibility(
+                isOnlyItem,
+                isLastItem,
+                viewTodayFeedbackCircle,
+                dividerTodayFeedbackExpression
+            )
         }
     }
 }
@@ -45,18 +67,34 @@ class FeedbackExpressionViewHolder(private val binding: ItemTodayFeedbackExpress
 class FeedbackGrammarViewHolder(private val binding: ItemTodayFeedbackGrammarBinding) :
     FeedbackViewHolder<TodayFeedback.Grammar>(binding.root) {
     init {
-        initItemViewClickListener(binding.spaceTodayFeedback, binding.layoutTodayFeedback.layoutTodayFeedback)
+        with(binding) {
+            initItemViewClickListener(
+                layoutTodayFeedbackGrammar.btnTalkFeedback,
+                spaceTodayFeedback,
+                layoutTodayFeedback.layoutTodayFeedback
+            )
+        }
     }
 
-    override fun onBind(feedbackData: TodayFeedback.Grammar, isLastItem: Boolean, isOnlyItem: Boolean) {
+    override fun onBind(
+        feedbackData: TodayFeedback.Grammar,
+        isLastItem: Boolean,
+        isOnlyItem: Boolean
+    ) {
         with(binding) {
+            layoutTodayFeedbackGrammar.btnTalkFeedback.visibility = View.VISIBLE
             layoutTodayFeedbackGrammar.tvMyFeedbackGrammarWrong.text = feedbackData.wrong
             layoutTodayFeedbackGrammar.tvMyFeedbackGrammarCorrect.text = feedbackData.correct
             layoutTodayFeedbackGrammar.tvMyFeedbackGrammarReason.text = feedbackData.reason
             layoutTodayFeedback.tvTodayFeedbackBefore.text = feedbackData.feedbackBefore
             layoutTodayFeedback.tvTodayFeedbackAfter.text = feedbackData.feedbackAfter
 
-            handleVisibility(isOnlyItem, isLastItem, viewTodayFeedbackCircle, dividerTodayFeedbackGrammar)
+            handleVisibility(
+                isOnlyItem,
+                isLastItem,
+                viewTodayFeedbackCircle,
+                dividerTodayFeedbackGrammar
+            )
         }
     }
 }
@@ -64,22 +102,43 @@ class FeedbackGrammarViewHolder(private val binding: ItemTodayFeedbackGrammarBin
 class FeedbackPronunciationViewHolder(private val binding: ItemTodayFeedbackPronunciationBinding) :
     FeedbackViewHolder<TodayFeedback.Pronunciation>(binding.root) {
     init {
-        initItemViewClickListener(binding.layoutTodayFeedbackPronunciation.groupTodayFeedbackPronunciation)
+        with(binding.layoutTodayFeedbackPronunciation) {
+            initItemViewClickListener(
+                btnTalkFeedback,
+                groupTodayFeedbackPronunciation
+            )
+        }
     }
 
-    override fun onBind(feedbackData: TodayFeedback.Pronunciation, isLastItem: Boolean, isOnlyItem: Boolean) {
+    override fun onBind(
+        feedbackData: TodayFeedback.Pronunciation,
+        isLastItem: Boolean,
+        isOnlyItem: Boolean
+    ) {
         val context = binding.root.context
 
         with(binding) {
-            layoutTodayFeedbackPronunciation.tvTodayFeedbackPronunciationWord.text = feedbackData.word
+            layoutTodayFeedbackPronunciation.btnTalkFeedback.visibility = View.VISIBLE
+            layoutTodayFeedbackPronunciation.tvTodayFeedbackPronunciationWord.text =  feedbackData.word
             layoutTodayFeedbackPronunciation.tvTodayFeedbackPronunciationWordPronun.text =
-                context.getString(R.string.tv_today_saved_pronunciation, feedbackData.pronunciation)
+                context.getString(
+                    R.string.tv_today_saved_pronunciation,
+                    feedbackData.pronunciation
+                )
             layoutTodayFeedbackPronunciation.tvTodayFeedbackPronunciationTranslation.text = feedbackData.translation
             layoutTodayFeedbackPronunciation.tvTodayFeedbackPronunciationSentence.text = feedbackData.sentence
             layoutTodayFeedbackPronunciation.tvTodayFeedbackPronunciationAccuracy.text =
-                context.getString(R.string.tv_today_saved_pronunciation_accuracy, feedbackData.accuracy)
+                context.getString(
+                    R.string.tv_today_saved_pronunciation_accuracy,
+                    feedbackData.accuracy
+                )
 
-            handleVisibility(isOnlyItem, isLastItem, viewTodayFeedbackCircle, dividerTodayFeedbackPronunciation)
+            handleVisibility(
+                isOnlyItem,
+                isLastItem,
+                viewTodayFeedbackCircle,
+                dividerTodayFeedbackPronunciation
+            )
         }
     }
 }
