3d9de39bb6d9f0cdd2a1fdf9337454323ef4d497
youjin09222 <youjin09222@naver.com>
2024-09-24 23:56:32 +0900

    #57 [FEAT] : ChallengeRecruitmentAdapter 클릭 이벤트 처리

--- app/src/main/java/com/talkable/presentation/challenge/ChallengeRecruitmentAdapter.kt
+++ app/src/main/java/com/talkable/presentation/challenge/ChallengeRecruitmentAdapter.kt
@@ -5,22 +5,29 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.talkable.databinding.ItemChallengeRecruitmentBinding
 
-class ChallengeRecruitmentAdapter(private val imgList: List<Recruitment>) :
-    RecyclerView.Adapter<RecruitmentViewHolder>() {
+class ChallengeRecruitmentAdapter(
+    private val imgList: List<Recruitment>,
+    private val onItemClicked: (Recruitment) -> Unit
+) : RecyclerView.Adapter<RecruitmentViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecruitmentViewHolder {
-        val binding =
-            ItemChallengeRecruitmentBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
+        val binding = ItemChallengeRecruitmentBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
         return RecruitmentViewHolder(binding)
     }
 
     override fun getItemCount() = imgList.size
 
     override fun onBindViewHolder(holder: RecruitmentViewHolder, position: Int) {
-        holder.bind(imgList[position])
+        val item = imgList[position]
+        holder.bind(item)
+
+        // 아이템 클릭 시 이벤트 처리
+        holder.itemView.setOnClickListener {
+            onItemClicked(item) // 클릭된 아이템 전달
+        }
     }
 }
