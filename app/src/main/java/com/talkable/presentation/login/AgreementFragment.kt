23879934f601f3a82ce1d4c23f29750886657702
youjin09222 <youjin09222@naver.com>
2024-10-25 03:25:19 +0900

    #81 [REFACTOR]: 전체 동의 로직 수정

--- app/src/main/java/com/talkable/presentation/login/AgreementFragment.kt
+++ app/src/main/java/com/talkable/presentation/login/AgreementFragment.kt
@@ -33,13 +33,41 @@ class AgreementFragment : BindingFragment<FragmentAgreementBinding>(R.layout.fra
     private fun initAgreementAllBtnClickListener() {
         binding.btnAgreementAll.setOnClickListener {
             with(binding) {
-                btnAgreementAge.isChecked = !btnAgreementAge.isChecked
-                btnAgreementTerms.isChecked = !btnAgreementTerms.isChecked
-                btnAgreementPrivacy.isChecked = !btnAgreementPrivacy.isChecked
-                btnAgreementMarketing.isChecked = !btnAgreementMarketing.isChecked
+                val isChecked = btnAgreementAll.isChecked
+                btnAgreementAge.isChecked = isChecked
+                btnAgreementTerms.isChecked = isChecked
+                btnAgreementPrivacy.isChecked = isChecked
+                btnAgreementMarketing.isChecked = isChecked
             }
-            //조건 검사 다시 해야함 일단 임시로 해둠
-            binding.btnAgreementStart.isEnabled = true
+            checkAllConditions()
+        }
+
+        // 각각의 체크 박스 상태 변경 시 전체 동의 체크박스 상태 반영
+        with(binding) {
+            val checkboxes = listOf(
+                btnAgreementAge,
+                btnAgreementTerms,
+                btnAgreementPrivacy,
+                btnAgreementMarketing
+            )
+
+            // 나머지 체크박스의 상태 변경 리스너 설정
+            checkboxes.forEach { checkBox ->
+                checkBox.setOnCheckedChangeListener { _, _ ->
+                    // 하나라도 체크 해제되면 btnAgreementAll도 체크 해제
+                    btnAgreementAll.isChecked = checkboxes.all { it.isChecked }
+                    checkAllConditions() // 조건에 맞춰 버튼 활성화
+                }
+            }
+        }
+    }
+
+    // 모든 필수 체크 박스 선택 시 버튼 활성화
+    private fun checkAllConditions() {
+        with(binding) {
+            btnAgreementStart.isEnabled = btnAgreementAge.isChecked &&
+                    btnAgreementTerms.isChecked &&
+                    btnAgreementPrivacy.isChecked
         }
     }
 }
