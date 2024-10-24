9a04ff0eb2f82542809b12f6db610471e94cdd18
youjin09222 <youjin09222@naver.com>
2024-10-24 11:40:51 +0900

    #80 [FEAT]: 버튼 클릭 시 가이드 넘어가게 변경

--- app/src/main/java/com/talkable/presentation/talk/TalkFragment.kt
+++ app/src/main/java/com/talkable/presentation/talk/TalkFragment.kt
@@ -186,7 +186,7 @@ class TalkFragment : BindingFragment<FragmentTalkBinding>(R.layout.fragment_talk
     private fun initTalkGuide() {
         if (guideClickCount == FIRST_CLICK) {
             with(binding) {
-                root.setOnClickListener {
+                btnTalkNext.setOnClickListener {
                     if (guideClickCount < MAX_GUIDE_CLICK) {
                         tvTalkEnglish.text = getString(englishGuideTextArray[textIndex])
                         tvTalkTranslate.text = getString(koreanGuideTextArray[textIndex])
@@ -194,13 +194,14 @@ class TalkFragment : BindingFragment<FragmentTalkBinding>(R.layout.fragment_talk
                         guideClickCount++
 
                     } else {
+                        btnTalkNext.isVisible = false
                         groupTalkSpeak.isVisible = true
                         btnTalkSpeak.isEnabled = true
                         tvTalkEnglish.isVisible = false
                         tvTalkTranslate.isVisible = false
                         tvTalkListen.isVisible = true
                         setQuestionLayout()
-                        binding.root.setOnClickListener { }
+                        binding.btnTalkNext.setOnClickListener { }
                     }
                 }
             }
