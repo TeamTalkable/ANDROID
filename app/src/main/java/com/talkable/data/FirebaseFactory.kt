6e43f15c2b5794c0f8fbff7ac504f212cb2c74a6
youjin09222 <youjin09222@naver.com>
2024-11-28 02:32:59 +0900

    #99 [ADD] : FirebaseFactory에 savedRef 추가

--- app/src/main/java/com/talkable/data/FirebaseFactory.kt
+++ app/src/main/java/com/talkable/data/FirebaseFactory.kt
@@ -3,14 +3,17 @@ package com.talkable.data
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.talkable.data.FirebaseKey.TALK_FEEDBACK
+import com.talkable.data.FirebaseKey.TALK_SAVED
 
 
 object FirebaseFactory {
     private val db = Firebase.database
     private val ref = db.getReference("0")
     val feedbackRef = ref.child(TALK_FEEDBACK)
+    val savedRef = ref.child(TALK_SAVED)
 }
 
 object FirebaseKey {
     const val TALK_FEEDBACK = "talk_feedback"
+    const val TALK_SAVED = "talk_saved"
 }
