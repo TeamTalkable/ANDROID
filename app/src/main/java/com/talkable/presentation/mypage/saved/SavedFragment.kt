cb31c7b24e64baebcd91199330f5e538f1156695
youjin09222 <youjin09222@naver.com>
2024-11-28 05:17:30 +0900

    #99 [DEL] : 필요없는 코드 제거

--- app/src/main/java/com/talkable/presentation/mypage/saved/SavedFragment.kt
+++ app/src/main/java/com/talkable/presentation/mypage/saved/SavedFragment.kt
@@ -4,16 +4,13 @@ import androidx.navigation.fragment.findNavController
 import com.google.android.material.tabs.TabLayoutMediator
 import com.talkable.R
 import com.talkable.core.base.BindingFragment
-import com.talkable.core.util.DialogKey
 import com.talkable.databinding.FragmentSavedBinding
-import com.talkable.presentation.quiz.QuizCategoryDialog
 
 class SavedFragment :
     BindingFragment<FragmentSavedBinding>(R.layout.fragment_saved) {
     override fun initView() {
         initBackBtnClickListener()
         initMyFeedbackViewPagerAdapter()
-        initQuizBtnClickListener()
     }
 
     private fun initBackBtnClickListener() {
@@ -29,13 +26,5 @@ class SavedFragment :
         TabLayoutMediator(binding.tlMyPageSaved, binding.vpMyPageSaved) { tab, position ->
             tab.text = SavedCategory.values()[position].label
         }.attach()
-    }
-
-    private fun initQuizBtnClickListener() {
-        binding.layoutMyPageSavedAppBar.ibMyPageWrite.setOnClickListener {
-            QuizCategoryDialog.createNewInstance().show(
-                childFragmentManager, DialogKey.QUIZ_DIALOG
-            )
-        }
     }
 }
